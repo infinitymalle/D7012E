04ac7225672222616637e2df67f6c64bd6bd3bb8
infinitymalle <malkolm.lundkvist@gmail.com>
2025-05-11 12:22:06 +0200

    Done P2

--- P2/P2.pl
+++ P2/P2.pl
@@ -1,5 +1,3 @@
-%smallestKset List K = write ("Sum\ti\tj\tsubList\n" ++ )
-
 listSum([], 0).
 listSum([Head | Tail], Sum) :-
     listSum(Tail, Restsum),
@@ -7,13 +5,21 @@ listSum([Head | Tail], Sum) :-
 
 firstKTuples([], _, []).
 firstKTuples(_, 0, []).
-firstKTuples([Head | Tail], K, [Head | Result]) :-
+firstKTuples([Head | Tail], K, _) :-
+    K > 0,
+    print_tuple(Head),
     K2 is K -1,
-    firstKTuples(Tail, K2, Result)
-
+    firstKTuples(Tail, K2, Result).
+    
 print_tuple((Sum, I, J, Sub)) :-
     format("~w\t~w\t~w\t~w~n", [Sum, I, J, Sub]).
 
+
+smallestKset(List, K) :-
+    allSubLists(List, Tuples),
+    insertionSort(Tuples, Sorted),
+    writeln("Sum\tI\tJ\tSublist"),
+    firstKTuples(Sorted, K, _).
 
 
 insertionSort([], []).
@@ -55,3 +61,32 @@ iterj(List, I, J, Len, [(Sum, I, J, Sub) | Rest]) :-
     listSum(Sub, Sum),
     J1 is J + 1,
     iterj(List, I, J1, Len, Rest).
+
+% Drop first I elements from list
+subListI(List, 0, List).
+subListI([_ | Tail], I, Result) :-
+    I > 0,
+    I1 is I - 1,
+    subListI(Tail, I1, Result).
+
+% Take first N elements from list
+subListJ(_, 0, []).
+subListJ([Head | Tail], N, [Head | Result]) :-
+    N > 0,
+    N1 is N - 1,
+    subListJ(Tail, N1, Result).
+subListJ([], _, []).  % guard for too-short input
+
+% Combine dropping and taking: sublist from I to J
+subList(List, I, J, Sub) :-
+    I >= 0,
+    J >= I,
+    subListI(List, I, Dropped),
+    Len is J - I + 1,
+    subListJ(Dropped, Len, Sub).
+
+
+
+
+% consult("P2.pl").
+% 
